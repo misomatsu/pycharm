a03281c200843750ec3aa85c502f14f391531734
misomatsu <kskrpt@yahoo.co.jp>
2024-05-02 21:08:10 +0900

    演算子の理解度チェック

--- rensyu.py
+++ rensyu.py
@@ -1,1 +1,29 @@
-print("hello msmt")
+# 問題1
+a = 8
+b = 4
+print(a/4)
+
+#問題2
+x = 10
+print((x + 3) * 2)
+
+#問題3
+c = 5
+d = 3
+print((c*d)-(c+d))
+#問題４ x = 10
+#      y = 3
+#      result = x ** y - y % x
+print("10*10*10-3 \n 1000-3 \n =997")
+
+#問題５ 5 + 3 * 2
+print("5+6 \n =11")
+
+#問題 ６a = True
+     #b = False
+     #c = False
+     #result = not a or b and not c or not (a and b)
+print("・not a はfalseを返している\n (not true) = false\n・b and not cはfalseを返している\n (false and true) = false"
+      "\n・not(a and b)はtrueを返している"
+      "\n (a and b)はtrue and false でfalseを返している"
+      "\n つまりnot false = true\n= false or false or true\n= true")
